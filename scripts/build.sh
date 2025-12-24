c56f44d4eeef1163f7d46319f31443a5723684c2
liuwenqing <liuwenqing@tianyancha.com>
2025-12-24 13:51:18 +0800

    commit

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+declare image='swr.cn-north-4.xxx.com/zs-base/sonarqube-enterprise:25.5-v1'
+declare image_latest='swr.cn-north-4.xxx.com/zs-base/cicd-base:latest'
+
+# login harbor:
+docker login -u cn-north-4@$ak -p $sk  swr.cn-north-4.myhuaweicloud.com
+docker build \
+    --no-cache \
+    -t ${image} \
+    -f Dockerfile \
+    .
+
+docker push ${image}
+
+# docker tag ${image} ${image_latest}
+
+# docker push ${image_latest}
